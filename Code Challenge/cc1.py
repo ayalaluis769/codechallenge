5f1d64f8e8f8212a3863f13fdd050bc5514bae67
ayalaluis769 <71358053+ayalaluis769@users.noreply.github.com>
2021-08-12 19:27:24 -0400

    Add files via upload

---
+++ Code Challenge/cc1.py
@@ -0,0 +1,15 @@
+#Turn this line into a comment
+
+cities = ["Seattle", "Tacoma", "Bellevue"]
+
+a = 1
+print(type(a))
+
+b = 107.25
+print(type(b))
+
+c = "Tony"
+print(type(c))
+
+d = a + b
+print(d)
